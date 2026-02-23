dd50bcb4427a76b1cf4ebc6d5068ed0da03f144a
Manish kumar <manishnkb7@gmail.com>
2026-02-23 05:59:20 +0530

    creating sustom hook for API fetching

---
+++ src/hooks/useProducts.js
@@ -0,0 +1,20 @@
+import { useState, useEffect } from "react";
+
+const useProducts = () => {
+  const [products, setProducts] = useState([]);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    fetch("https://dummyjson.com/products")
+      .then(res => {
+        if (!res.ok) throw new Error("Failed to fetch products");
+        return res.json();
+      })
+      .then(data => setProducts(data.products))
+      .catch(err => setError(err.message));
+  }, []);
+
+  return { products, error };
+};
+
+export default useProducts;
